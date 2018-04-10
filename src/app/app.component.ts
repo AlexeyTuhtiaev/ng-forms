38ac1207b8b419f092058b866a26c7650d1190fc
alexey.tuhtiaev <alexey.tuhtiaev@gmail.com>
2018-04-10 18:49:05 +0300

    create async validator by using Promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {reject} from "q";
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,7 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.form = new FormGroup({
       user: new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
+        email: new FormControl('', [Validators.required, Validators.email], [this.checkForEmail]),
         pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)])
       }),
       country: new FormControl('ru'),
@@ -38,5 +39,19 @@ export class AppComponent implements OnInit{
       };
     }
     return null;
+  }
+
+  checkForEmail(control: FormControl): Promise<any>{
+    return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+        if(control.value === 'test@test.by'){
+          resolve({
+            'emailIsUsed': true
+          });
+        } else {
+          resolve(null);
+        }
+      }, 3000)
+    });
   }
 }
